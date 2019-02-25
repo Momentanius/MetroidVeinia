eb56342d1976118399acc6e19c86ff8c9236f29c
Momentanius <guilhermegustavogohr@gmail.com>
2019-02-25 15:57:25 -0300

    Novos movimentos pro Richter

--- Project/Scripts/Player.gd
+++ Project/Scripts/Player.gd
@@ -4,23 +4,28 @@ var motion = Vector2()
 
 var speed = 140
 var direction = true #Se true, é direita
-var falling
+var is_falling = false
+var is_attacking = false
+
+var jump_max = 0
 
 const GRAVITY = 600
-const JUMP_FORCE = 400
+const JUMP_FORCE = 250
 const UP = Vector2(0, -1) #Para que o salto seja para cima
 
 func _physics_process(delta):
 	fall(delta)
 	move()
 	jump()
+	down()
+	attack()
 	move_and_slide(motion, UP)
 
 func fall(delta):
 	if is_on_floor() or is_on_ceiling():
 		motion.y = 0
 	else:
-		if falling:
+		if is_falling:
 			play_animation('fall')
 		motion.y += delta * GRAVITY
 
@@ -34,23 +39,44 @@ func flipar():
 		$AnimatedSprite.flip_h = true
 
 func move():
-	if Input.is_action_pressed('ui_right') and not Input.is_action_pressed('ui_left'):
-		direction = true
-		flipar()
-		play_animation('walk')
-		motion.x = speed
-	elif Input.is_action_pressed('ui_left') and not Input.is_action_pressed('ui_right'):
-		direction = false
-		flipar()
-		play_animation('walk')
-		motion.x = -speed
-	else:
-		play_animation('idle')
+	if !is_attacking:
+		if Input.is_action_pressed('ui_right') and not Input.is_action_pressed('ui_left'):
+			direction = true
+			flipar()
+			play_animation('walk')
+			motion.x = speed
+		elif Input.is_action_pressed('ui_left') and not Input.is_action_pressed('ui_right'):
+			direction = false
+			flipar()
+			play_animation('walk')
+			motion.x = -speed
+		else:
+			play_animation('idle')
+			motion.x = 0
+
+func attack():
+	if Input.is_action_just_pressed('ui_attack') and !is_attacking:
 		motion.x = 0
+		$AttackSFX.play()
+		$AttackTimer.start()
+		is_attacking = true
+		play_animation('attack')
+
+func down():
+	if Input.is_action_just_pressed('ui_down') and not Input.is_action_pressed('ui_left') and not Input.is_action_pressed('ui_right') and not Input.is_action_pressed('ui_up'):
+		motion.x = 0
+		play_animation('get_down')
+	elif Input.is_action_pressed('ui_down') and not Input.is_action_pressed('ui_left') and not Input.is_action_pressed('ui_right') and not Input.is_action_pressed('ui_up'):
+		play_animation('duck')
+
 
 func jump():
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
-			play_animation('jump')
 			motion.y = -JUMP_FORCE
-			play_animation('jump')
+	if motion.y < 0:
+		play_animation('jump')
+
+
+func _on_AttackTimer_timeout():
+	is_attacking = false
